09424df5fb7f4bc2c8dcc6733e4cfb13b069453e
Juri Kivimae <kivimae@gmail.com>
2023-02-23 22:23:24 +0200

    routes wip

--- web/routes.py
+++ web/routes.py
@@ -1,19 +1,27 @@
 
 from flask import Blueprint, jsonify, request, render_template
-from models import db, Company, NaturalShareHolder, LegalShareHolder
+
+from web.models import db, Company, NaturalShareHolder, LegalShareHolder
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def hello():
-    return render_template('index.html'), 200
+    companies = Company.query.all()
+    return render_template('index.html', companies=companies), 200
 
 @main_bp.route('/company/<int:id>', methods=['GET'])
 def get_company(id):
     company = Company.query.get(id)
     if company is None:
         return jsonify({'message': 'Company not found'}), 404
-    return jsonify(company.to_json()), 200
+    legal_shareholders = LegalShareHolder.query.filter_by(company_id=company.reg_code).all()
+    natural_shareholders = NaturalShareHolder.query.filter_by(company_id=company.reg_code).all()
+    if legal_shareholders is not None:
+         company.legal_shareholders = legal_shareholders
+    if natural_shareholders is not None:
+        company.natural_shareholders = natural_shareholders
+    return render_template('details.html', company=company), 200
 
 @main_bp.route('/company/list', methods=['GET'])
 def get_companies():
@@ -28,7 +36,6 @@ def create_company():
     company = Company(
         name=request.json['name'],
         reg_code=request.json['reg_code'],
-        start_date=request.json['start_date'],
         start_capital=request.json['start_capital']
     )
     db.session.add(company)
