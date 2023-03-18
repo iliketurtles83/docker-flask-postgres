9c9faf369abd9c3b35bc0277df1b387fc4414cd2
Juri Kivimae <kivimae@gmail.com>
2023-03-18 21:16:04 +0200

    rename var to let

--- web/static/js/script.js
+++ web/static/js/script.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
     function incrementIdName(template) {
         template.find('input').each(function() {
-            var name = $(this).attr('name');
-            var id = $(this).attr('id');
-            var index = name.match(/\d+/)[0];
-            var new_index = parseInt(index) + 1;
+            let name = $(this).attr('name');
+            let id = $(this).attr('id');
+            let index = name.match(/\d+/)[0];
+            let new_index = parseInt(index) + 1;
             $(this).attr('name', name.replace(index, new_index));
             $(this).attr('id', id.replace(index, new_index));
         });
     }
 
     $('#add-natural-shareholder').click(function() {
-        var template = $('.natural-shareholder-form:first').clone();
+        let template = $('.natural-shareholder-form:first').clone();
         console.log(template)
         template.find('input').val('');
         // make founder checkbox unchecked
@@ -26,7 +26,7 @@ $(document).ready(function() {
 
 
     $('#add-legal-shareholder').click(function() {
-        var template = $('.legal-shareholder-form:first').clone();
+        let template = $('.legal-shareholder-form:first').clone();
         template.find('input').val('');
         // make founder checkbox unchecked
         template.find('input[type=checkbox]').prop('checked', false);
@@ -40,4 +40,5 @@ $(document).ready(function() {
     $(document).on('click', '.remove-shareholder', function() {
         $(this).parent().remove();
     });
+
 });
